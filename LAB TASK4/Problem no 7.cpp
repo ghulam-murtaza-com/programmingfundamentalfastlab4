b2d8b5d80a99e469fdacc8adb744c042c8edc4bc
ghulam-murtaza-com <perhlybhai@gmail.com>
2024-09-14 10:25:33 +0500

    Add files via upload

---
+++ LAB TASK4/Problem no 7.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\t\t\t\t ________________________\n");
+    printf("\t\t\t\t/\t\t\t \\\n");
+    printf("\t\t\t       /       / \\_/ \\\t\t  \\\n");
+    printf("\t\t\t      /       ( .  .  )\t  ^        \\\n");
+    printf("\t\t\t     /         )     (\t  ((        \\\n");	
+    printf("\"cat\"          \t\t    |         ( >   < )  _((         |");
+    printf("\n\t\t  \t     \\\t       -------\t    \t    /");
+    printf("\n\t\t\t      \\\t      24K-0720\t\t   /");
+    printf("\n\t\t\t       \\\t      \t\t  /");
+    printf("\n\t\t\t\t\\________________________/");
+
+    return 0;
+}
+
