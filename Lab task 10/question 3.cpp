c05f7d1fdfbaf73f493fc62c14c06432280370c9
ghulam-murtaza-com <perhlybhai@gmail.com>
2024-11-20 14:39:57 +0500

    Update question 3.cpp

--- Lab task 10/question 3.cpp
+++ Lab task 10/question 3.cpp
@@ -1,53 +1,96 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 100
+struct Pkg {
+    char name[30];
+    char destination[30];
+    int duration;
+    float cost;
+    int seats;
+} packages[50];
 
-struct Car {
-    char mk[20];
-    char mdl[20];
-    int yr;
-    float prc;
-    float mlg;
-} crs[MAX];
+int packageCount = 0;
 
-int cnt = 0;
-
-void add() {
-    if (cnt < MAX) {
-        printf("Make: "); scanf("%s", crs[cnt].mk);
-        printf("Model: "); scanf("%s", crs[cnt].mdl);
-        printf("Year: "); scanf("%d", &crs[cnt].yr);
-        printf("Price: "); scanf("%f", &crs[cnt].prc);
-        printf("Mileage: "); scanf("%f", &crs[cnt].mlg);
-        cnt++;
-    } else printf("Limit reached.\n");
+void addPackage() {
+    if (packageCount < 50) {
+        printf("Enter Package Name: ");
+        scanf("%s", packages[packageCount].name);
+        printf("Enter Destination: ");
+        scanf("%s", packages[packageCount].destination);
+        printf("Enter Duration (in days): ");
+        scanf("%d", &packages[packageCount].duration);
+        printf("Enter Cost: ");
+        scanf("%f", &packages[packageCount].cost);
+        printf("Enter Available Seats: ");
+        scanf("%d", &packages[packageCount].seats);
+        packageCount++;
+        printf("Package added successfully.\n");
+    } else {
+        printf("Cannot add more packages. Limit reached.\n");
+    }
 }
 
-void disp() {
-    for (int i = 0; i < cnt; i++)
-        printf("\n%s %s %d $%.2f %.2f miles\n", crs[i].mk, crs[i].mdl, crs[i].yr, crs[i].prc, crs[i].mlg);
+void displayPackages() {
+    if (packageCount == 0) {
+        printf("No packages available.\n");
+        return;
+    }
+    printf("\nAvailable Travel Packages:\n");
+    for (int i = 0; i < packageCount; i++) {
+        if (packages[i].seats > 0) {
+            printf("Package: %s, Destination: %s, Duration: %d days, Cost: $%.2f, Seats Available: %d\n",
+                   packages[i].name, packages[i].destination, packages[i].duration, packages[i].cost, packages[i].seats);
+        }
+    }
 }
 
-void srch() {
-    char srh[20];
-    printf("Search make/model: "); scanf("%s", srh);
-    for (int i = 0; i < cnt; i++) 
-        if (strstr(crs[i].mk, srh) || strstr(crs[i].mdl, srh))
-            printf("\n%s %s %d $%.2f %.2f miles\n", crs[i].mk, crs[i].mdl, crs[i].yr, crs[i].prc, crs[i].mlg);
+void bookPackage() {
+    char searchName[30];
+    printf("Enter the package name to book: ");
+    scanf("%s", searchName);
+
+    for (int i = 0; i < packageCount; i++) {
+        if (strcmp(packages[i].name, searchName) == 0) {
+            if (packages[i].seats > 0) {
+                packages[i].seats--;
+                printf("Package booked successfully. Remaining seats: %d\n", packages[i].seats);
+                return;
+            } else {
+                printf("No seats available for this package.\n");
+                return;
+            }
+        }
+    }
+    printf("Package not found.\n");
 }
 
 int main() {
-    int ch;
+    int choice;
+
     while (1) {
-        printf("\n1. Add 2. Show 3. Search 4. Exit\nChoice: ");
-        scanf("%d", &ch);
-        switch (ch) {
-            case 1: add(); break;
-            case 2: disp(); break;
-            case 3: srch(); break;
-            case 4: return 0;
-            default: printf("Invalid.\n");
+        printf("\nMenu:\n");
+        printf("1. Add Package\n");
+        printf("2. Display Packages\n");
+        printf("3. Book Package\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                addPackage();
+                break;
+            case 2:
+                displayPackages();
+                break;
+            case 3:
+                bookPackage();
+                break;
+            case 4:
+                printf("Exiting program. Goodbye!\n");
+                return 0;
+            default:
+                printf("Invalid choice. Please try again.\n");
         }
     }
 }
